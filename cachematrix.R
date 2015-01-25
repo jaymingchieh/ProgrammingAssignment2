8aea3e61962aaeb312f8e651370870ad5d391606
jaycheng <Shao@Chengde-iMac-3.local>
2015-01-25 16:13:44 +0800

    Assignment submit
    this is No.1 submit

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,31 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+    m <- NULL
+    get <- function() x
+    setImatrix <- function(Imatrix) m <<- Imatrix
+    getImatrix <- function() m
+    
+    # return a list of functions as an R object
+    list(get=get, setImatrix=setImatrix, getImatrix=getImatrix)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getImatrix()
+    if(!is.null(m)){
+        message("Cached data found. Getting result... Done.")
+        return(m)
+    }
+    else {
+        message("No cached data found. Calculating inverse matrix...")
+        data <- x$get() 
+        m <- solve(data) 
+        x$setImatrix(m) 
+        message("Done.")
+        return(m)
+    }
 }
